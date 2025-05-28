bd80b2bab2064fab0dc0c65c9b25ae32a5da6262
tanvi-rm <tanvimane322@gmail.com>
2025-05-28 12:27:10 +0530

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace EmployeeWebService
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+}
